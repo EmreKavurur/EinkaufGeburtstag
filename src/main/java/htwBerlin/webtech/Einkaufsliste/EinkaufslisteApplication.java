a17d3efbed9974e5e8571b1698b2f0fcc039c1de
emrekavurur <emre.kavurur@student-htw.berlin.de>
2023-10-20 14:02:58 +0200

    init

---
+++ src/main/java/htwBerlin/webtech/Einkaufsliste/EinkaufslisteApplication.java
@@ -0,0 +1,13 @@
+package htwBerlin.webtech.Einkaufsliste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EinkaufslisteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EinkaufslisteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/htwBerlin/webtech/Einkaufsliste/EinkaufslisteApplicationTests.java
@@ -0,0 +1,13 @@
+package htwBerlin.webtech.Einkaufsliste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EinkaufslisteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
