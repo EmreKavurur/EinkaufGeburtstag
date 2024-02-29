6bd4f57d9644c0a805ed83fae9f3e13314878f40
emrekavurur <emre.kavurur@student-htw.berlin.de>
2024-02-29 13:26:42 +0100

    M1/M2 Mission Lombok about to start

--- src/main/java/htwBerlin/webtech/Einkaufsliste/web/Item.java
+++ src/main/java/htwBerlin/webtech/Einkaufsliste/web/Item.java
@@ -5,14 +5,29 @@ import lombok.*;
 
 @Entity
 @Table(name="item")
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+
 public class Item {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="id")
     private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Column(name="name")
     private String name;
 
